8bc04a47b4575c857cd802a9bff5d6738c2987d0
Ra3on <ramondesantiago30@gmail.com>
2024-01-31 15:41:52 -0800

    Initial Commit

---
+++ Login And SignUp/Login.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  Sparring
+//
+//  Created by Ramon De Santiago on 1/31/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
